712e2f0a58fc57e3a70bd12f855bd0f5c11b4d28
dreyna <reyna_b@upeu.edu.pe>
2022-10-04 12:28:43 -0500

    v1.5

--- app/src/main/java/com/example/api/utils/Apis.java
+++ app/src/main/java/com/example/api/utils/Apis.java
@@ -2,7 +2,7 @@ package com.example.api.utils;
 
 public class Apis {
 
-    public static final String URL_001="http://192.168.1.2:8080/personas/";
+    public static final String URL_001="http://172.17.7.24:8080/personas/";
 
     public static com.example.api.utils.PersonaService getPersonaService(){
         return  Cliente.getClient(URL_001).create(com.example.api.utils.PersonaService.class);
